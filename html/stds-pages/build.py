ed6c7fe32470983a3aaa206ac2e6823eb3408717
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2019-05-23 09:01:40 -0500

    clean up and comment build script

--- html/stds-pages/build.py
+++ html/stds-pages/build.py
@@ -2,49 +2,25 @@ import csv
 import os
 import sys
 
+# reads a CSV file via a local file path and returns a list of dicts where headers are keys
+def readCsvAsDicts(path):
+    dictList = []
+    with open(path, 'r', newline='', encoding='utf-8') as fileObject:
+        readerObject = csv.DictReader(fileObject)
+        for row in readerObject:
+            dictList.append(row)
+    return dictList
+
+# read metadata from local clone of rs.tdwg.org repo
+standards = readCsvAsDicts('../../standards/standards.csv')
+standardsParts = readCsvAsDicts('../../standards/standards-parts.csv')
+docs = readCsvAsDicts('../../docs/docs.csv')
+contributors = readCsvAsDicts('../../docs/docs-authors.csv')
+vocabs = readCsvAsDicts('../../vocabularies/vocabularies.csv')
+pageInfo = readCsvAsDicts('pageInfo.csv')
+
+# These list items serve both as the keys in the header section and column headers in pageInfo.csv
 headerItems = ['summary','cover_image', 'cover_image_by', 'cover_image_ref', 'tags', 'github', 'website', 'website_title', 'status']
-
-# read standards metadata from rs.tdwg.org repo
-standards = []
-with open('../../standards/standards.csv', 'r', newline='', encoding='utf-8') as fileObject:
-    readerObject = csv.DictReader(fileObject)
-    for row in readerObject:
-        standards.append(row)
-
-# read parts of standards from rs.tdwg.org repo
-standardsParts = []
-with open('../../standards/standards-parts.csv', 'r', newline='', encoding='utf-8') as fileObject:
-    readerObject = csv.DictReader(fileObject)
-    for row in readerObject:
-        standardsParts.append(row)
-
-# read docs metadata from rs.tdwg.org repo
-docs = []
-with open('../../docs/docs.csv', 'r', newline='', encoding='utf-8') as fileObject:
-    readerObject = csv.DictReader(fileObject)
-    for row in readerObject:
-        docs.append(row)
-
-# read contributor metadata from rs.tdwg.org repo
-contributors = []
-with open('../../docs/docs-authors.csv', 'r', newline='', encoding='utf-8') as fileObject:
-    readerObject = csv.DictReader(fileObject)
-    for row in readerObject:
-        contributors.append(row)
-
-# read vocabs metadata from rs.tdwg.org repo
-vocabs = []
-with open('../../vocabularies/vocabularies.csv', 'r', newline='', encoding='utf-8') as fileObject:
-    readerObject = csv.DictReader(fileObject)
-    for row in readerObject:
-        vocabs.append(row)
-
-# read additional data to build web page
-pageInfo = []
-with open('pageInfo.csv', 'r', newline='', encoding='utf-8') as fileObject:
-    readerObject = csv.DictReader(fileObject)
-    for row in readerObject:
-        pageInfo.append(row)
 
 # create a directory for output if it doesn't already exist
 try:
@@ -52,35 +28,37 @@ try:
 except:
     pass
 
-for pageIndex in range(0,len(pageInfo)):
-#for pageIndex in range(21, 22):
+# loop through the rows of the pageInfo CSV table and generate a Markdown page for each
+for page in pageInfo:
+
     # match this page with its record in the standards metadata
     found = False
     for standardIndex in range(0, len(standards)):
-        if pageInfo[pageIndex]['standard'] == standards[standardIndex]['standard']:
+        if page['standard'] == standards[standardIndex]['standard']:
             found = True
             standardNumber = standardIndex
 
-    # if successful in finding a standards record, build the Markdown
+    # if successful in finding a standards metadata record, build the Markdown
     if found:
-        # uncomment the triple single-quotes block to suppress file output
-        #'''
-        # create a directory for output if it doesn't already exist
+        # create a directory for the standard if it doesn't already exist
         try:
-            os.mkdir('output/' + pageInfo[pageIndex]['directoryName'])
+            os.mkdir('output/' + page['directoryName'])
         except:
             pass
 
-        outObject = open('output/' + pageInfo[pageIndex]['directoryName'] + '/index.md', 'wt', encoding='utf-8')
-        #'''        
-        #outObject = sys.stdout # uncomment this line to output to the console
+        # opent the index.md file in the standard's directory
+        outObject = open('output/' + page['directoryName'] + '/index.md', 'wt', encoding='utf-8')
         
+        # print the Jekyll metadata header section
         print('---', file=outObject)
+        # use the official standards title for now, perhaps chage to shortened version
         print('title: ' + standards[standardNumber]['label'], file=outObject)
         for headerItem in headerItems:
-            if pageInfo[pageIndex][headerItem] != '':
-                print(headerItem + ': ' + pageInfo[pageIndex][headerItem], file=outObject)
+            if page[headerItem] != '':
+                print(headerItem + ': ' + page[headerItem], file=outObject)
         print('---', file=outObject)
+
+        # print the header section required by the SDS
         print('', file=outObject)
         print('## Header section', file=outObject)
         print('', file=outObject)
@@ -96,18 +74,22 @@ for pageIndex in range(0,len(pageInfo)):
         print('Category', file=outObject)
         print(': ' + standards[standardNumber]['category'], file=outObject)
         print('', file=outObject)
+
+        # describe the parts of the standard (vocabularies and documents)
         print('## Parts of the standard', file=outObject)
         print('', file=outObject)
 
-        # find vocabularies and documents that are part of the standard
+        # make lists of documents and vocabularies that are part of the standard 
         docsInStd = []
         vocabsInStd = []
         for part in standardsParts:
-            if part['standard'] == pageInfo[pageIndex]['standard']:
+            if part['standard'] == page['standard']:
                 if part['rdf_type'] == 'foaf:Document':
                     docsInStd.append(part['part'])
                 elif part['rdf_type'] == 'tdwgutility:Vocabulary':
                     vocabsInStd.append(part['part'])
+
+        # summarize the number of vocabularies and documents in the standard
         sentence = 'This standard is comprised of '
         if len(vocabsInStd) > 1:
             sentence += str(len(vocabsInStd)) + ' vocabularies and '
@@ -125,6 +107,7 @@ for pageIndex in range(0,len(pageInfo)):
             print('Vocabularies:', file=outObject)
             print('', file=outObject)
             for vocabToList in vocabsInStd:
+                # find the current vocabulary in the vocabularies metadata
                 for vocab in vocabs:
                     if vocab['vocabulary'] == vocabToList:
                         print(vocab['label'] + ' (<' + vocab['vocabulary'] + '>)', file=outObject)
@@ -134,22 +117,24 @@ for pageIndex in range(0,len(pageInfo)):
         print('Documents:', file=outObject)
         print('', file=outObject)
         for docToList in docsInStd:
-                for doc in docs:
-                    if doc['current_iri'] == docToList:
-                        print('**Title:** ' + doc['documentTitle'] + '\\', file=outObject)
-                        print('**IRI:** [' + doc['current_iri'] + '](' + doc['browserRedirectUri'] + ')\\', file=outObject)
-                        print('**Created:** ' + doc['doc_created'] + '\\', file=outObject)
-                        print('**Last modified:** ' + doc['doc_modified'] + '\\', file=outObject)
-                        print('**Contributors:**\\', file=outObject)
-                        for contributor in contributors:
-                            if contributor['document'] == docToList:
-                                listing = contributor['contributor_literal'] + ' (' + contributor['contributor_role'] + ')'
-                                if contributor['affiliation'] != '':
-                                    listing += ' - ' + contributor['affiliation']
-                                print(listing + '\\', file=outObject)
-                        print('**Publisher:** ' + doc['publisher'] + '\\', file=outObject)
-                        print('**Abstract:** ' + doc['abstract'] + '\\', file=outObject)
-                        if doc['comment'] != '':
-                            print('**Note:** ' + doc['comment'] + '\\', file=outObject)
-                        print('**Citation:** ' + doc['citation'], file=outObject)
-                        print('', file=outObject)
+            # find the current document in the documents metadata
+            for doc in docs:
+                if doc['current_iri'] == docToList:
+                    # print metadata about each document
+                    print('**Title:** ' + doc['documentTitle'] + '\\', file=outObject)
+                    print('**IRI:** [' + doc['current_iri'] + '](' + doc['browserRedirectUri'] + ')\\', file=outObject)
+                    print('**Created:** ' + doc['doc_created'] + '\\', file=outObject)
+                    print('**Last modified:** ' + doc['doc_modified'] + '\\', file=outObject)
+                    print('**Contributors:**\\', file=outObject)
+                    for contributor in contributors:
+                        if contributor['document'] == docToList:
+                            listing = contributor['contributor_literal'] + ' (' + contributor['contributor_role'] + ')'
+                            if contributor['affiliation'] != '':
+                                listing += ' - ' + contributor['affiliation']
+                            print(listing + '\\', file=outObject)
+                    print('**Publisher:** ' + doc['publisher'] + '\\', file=outObject)
+                    print('**Abstract:** ' + doc['abstract'] + '\\', file=outObject)
+                    if doc['comment'] != '':
+                        print('**Note:** ' + doc['comment'] + '\\', file=outObject)
+                    print('**Citation:** ' + doc['citation'], file=outObject)
+                    print('', file=outObject)
